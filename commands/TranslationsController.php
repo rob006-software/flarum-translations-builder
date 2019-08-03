6020bf52c7ea9bd5eb274760d885c838b746c2fe
Robert Korulczyk <robert@korulczyk.pl>
2019-08-03 17:13:36 +0200

    Allow to specify projects updated by `translations/update` command.

--- commands/TranslationsController.php
+++ commands/TranslationsController.php
@@ -42,16 +42,18 @@ class TranslationsController extends Controller {
 		]);
 	}
 
-	public function actionUpdate(string $configFile = '@app/translations/config.php') {
+	public function actionUpdate(?array $projects = null, string $configFile = '@app/translations/config.php') {
 		$translations = $this->getTranslations($configFile);
 		$token = __METHOD__ . '#' . $translations->getHash();
 		if ($this->isLimited($token)) {
 			return;
 		}
 		foreach ($translations->getProjects() as $project) {
-			$catalogue = $project->updateSources();
-			foreach ($project->getLanguages() as $language) {
-				$project->updateComponents($language, $catalogue);
+			if ($projects === null || in_array($project->getId(), $projects, true)) {
+				$catalogue = $project->updateSources();
+				foreach ($project->getLanguages() as $language) {
+					$project->updateComponents($language, $catalogue);
+				}
 			}
 		}
 
