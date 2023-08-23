44097c45e51d5b3fe12bb28c3d3eff2c6c658f32
Robert Korulczyk <robert@korulczyk.pl>
2023-08-23 20:10:52 +0200

    Revert "Add more logging for fetching stats from Packagist in `StatsRepository`."
    This reverts commit 555a7dd0

--- components/extensions/StatsRepository.php
+++ components/extensions/StatsRepository.php
@@ -18,7 +18,6 @@ use app\models\PremiumExtension;
 use Yii;
 use yii\base\Component;
 use function date;
-use function json_encode;
 use function strtotime;
 use function time;
 
@@ -42,8 +41,6 @@ class StatsRepository extends Component {
 
 	private function getStatsFromPackagist(string $name): PackagistStats {
 		return Yii::$app->cache->getOrSet($this->buildStatsKey($name), static function () use ($name) {
-			// @todo this is temporary logging to debug some weird stats behavior
-			Yii::info("Fetch packagist stats for $name: " . json_encode($stats['downloads'] ?? [], JSON_THROW_ON_ERROR), 'packagist.stats');
 			$stats = Yii::$app->extensionsRepository->getPackagistData($name);
 			if ($stats === null || empty($stats['downloads'])) {
 				return new PackagistStats([]);
