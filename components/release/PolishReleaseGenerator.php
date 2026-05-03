0a02b14c5f0017aca8251b5c850f3aad3312ef53
Robert Korulczyk <robert@korulczyk.pl>
2026-05-03 20:31:29 +0200

    Deprecate `PolishReleaseGenerator`

--- components/release/PolishReleaseGenerator.php
+++ components/release/PolishReleaseGenerator.php
@@ -13,26 +13,13 @@ declare(strict_types=1);
 
 namespace app\components\release;
 
-use app\models\Repository;
-use function strncmp;
-
 /**
  * Class PolishReleaseGenerator.
+ *
+ * @deprecated Use {@link ReleaseGenerator} instead.
  *
  * @author Robert Korulczyk <robert@korulczyk.pl>
  */
 final class PolishReleaseGenerator extends ReleaseGenerator {
 
-	protected function getCoreChanges(): array {
-		$coreChanges = parent::getCoreChanges();
-		// treat changes inside `less/` dir as styles changes
-		foreach ($this->getChangedFiles() as $file => $changeType) {
-			if (strncmp('less/', $file, 5) === 0) {
-				$coreChanges['config.css'] = Repository::CHANGE_MODIFIED;
-				break;
-			}
-		}
-
-		return $coreChanges;
-	}
 }
